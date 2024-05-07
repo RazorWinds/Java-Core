11341bf3bb8bfcfdc04824ea0d8f5f487ba3682a
Logan Marshall <logan.marshall.dev@gmail.com>
2024-05-07 14:55:18 +0100

    Added the java project to the repository

---
+++ JavaCore/src/main/java/com/spartaglobal/javacore/JavaCore.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.spartaglobal.javacore;
+
+/**
+ *
+ * @author sword
+ */
+public class JavaCore {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
