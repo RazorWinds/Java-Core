26b9c40bd84526d613d423a0711cac2698466814
Logan Marshall <logan.marshall.dev@gmail.com>
2024-05-08 13:10:28 +0100

    Fixed @DisplayName

--- refactoring/src/test/java/AppTests.java
+++ refactoring/src/test/java/AppTests.java
@@ -1,6 +1,5 @@
 import com.spartaglobal.refactoring.Refactoring;
-import org.junit.jupiter.api.Assertions;   
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,12 +20,13 @@ public class AppTests {
 
     
     @Test
+    @DisplayName("TOD message boundry tests")
     void timeBoundryTest(){
-        int[] times = {11, 12, 18, 19};
-        Assertions.assertEquals("Good morning", Refactoring.getMessage(times[0]));
-        Assertions.assertEquals("Good afternoon", Refactoring.getMessage(times[1]));
-        Assertions.assertEquals("Good afternoon", Refactoring.getMessage(times[2]));
-        Assertions.assertEquals("Good evening", Refactoring.getMessage(times[3]));
+        int[] boundries = {11, 12, 18, 19};
+        Assertions.assertEquals("Good morning", Refactoring.getMessage(boundries[0]));
+        Assertions.assertEquals("Good afternoon", Refactoring.getMessage(boundries[1]));
+        Assertions.assertEquals("Good afternoon", Refactoring.getMessage(boundries[2]));
+        Assertions.assertEquals("Good evening", Refactoring.getMessage(boundries[3]));
     }
     
 
