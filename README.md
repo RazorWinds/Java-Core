855605ffddfcea39d34bc4d692cb4dad727eca15
Logan Marshall <logan.marshall.dev@gmail.com>
2024-05-08 13:50:05 +0100

    Added some questions to readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Java-Core
 Java project for following in class
+
+
+##Questions
+Ticked when answered
+- [ ] What is maven?
+- [ ] How does maven differ from standard Java?
+- [ ] What does POM stand for?
--- refactoring/src/test/java/AppTests.java
+++ refactoring/src/test/java/AppTests.java
@@ -2,6 +2,8 @@ import com.spartaglobal.refactoring.Refactoring;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 
@@ -30,4 +32,26 @@ public class AppTests {
     }
     
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 11})
+    @DisplayName("@Parameterized - Good morning")
+    void timeBoundryTest2(int timeBoundry){
+        Assertions.assertEquals("Good morning", Refactoring.getMessage(timeBoundry));
+    }
+   
+    
+    @ParameterizedTest
+    @ValueSource(ints = {12, 15, 18})
+    @DisplayName("@Parameterized - Good afternoon")
+    void timeBoundryTest3(int timeBoundry){
+        Assertions.assertEquals("Good afternoon", Refactoring.getMessage(timeBoundry));
+    }
+    
+    
+    @ParameterizedTest
+    @ValueSource(ints = {19, 20})
+    @DisplayName("@Parameterized - Good evening")
+    void timeBoundryTest4(int timeBoundry){
+        Assertions.assertEquals("Good evening", Refactoring.getMessage(timeBoundry));
+    }
 }
